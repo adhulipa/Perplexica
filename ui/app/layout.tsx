79364c1cfb5651e07ca613b04715c8cd39c8ac81
Aditya Dhulipala <aditya.d@hotmail.com>
2024-10-16 22:48:11 -0700

    style(app): use a grotesk font for better readability

--- ui/app/layout.tsx
+++ ui/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from 'next';
-import { Montserrat } from 'next/font/google';
+import { Hanken_Grotesk } from 'next/font/google';
 import './globals.css';
 import { cn } from '@/lib/utils';
 import Sidebar from '@/components/Sidebar';
 import { Toaster } from 'sonner';
 import ThemeProvider from '@/components/theme/Provider';
 
-const montserrat = Montserrat({
+const font = Hanken_Grotesk({
   weight: ['300', '400', '500', '700'],
   subsets: ['latin'],
   display: 'swap',
@@ -26,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html className="h-full" lang="en" suppressHydrationWarning>
-      <body className={cn('h-full', montserrat.className)}>
+      <body className={cn('h-full', font.className)}>
         <ThemeProvider>
           <Sidebar>{children}</Sidebar>
           <Toaster
